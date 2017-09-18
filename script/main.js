1ffc0a496de5bd08cf70144cb2d6546ce181b664
Pozeg <s_pozeg@tss-hr420-dm32.local>
2017-09-18 17:22:50 -0400

    adding functionality, content

--- script/main.js
+++ script/main.js
@@ -7,7 +7,40 @@
 
       // i want to change all the content on the page
       function changeElements() {
-        debugger; // this is a special term that stops code execution
+        //debugger; // this is a special term that stops code execution
+        let subImages = document.querySelector('.subImagesContainer');
+        let objectIndex = dynamicContent[this.id];
+
+        // remove duplicate images
+        while (subImages.firstChild) {
+          subImages.removeChild(subImages.firstChild);
+        }
+
+        // add the images to the bottom of the page
+        objectIndex.images.forEach(function(image, index) {
+          // create an image element
+          let newSubImg = document.createElement('img');
+          // add a css class to it
+          newSubImg.classList.add('thumb');
+          // set the src
+          newSubImg.src = "imges/" + pbjectIndex.images[index];
+          // add it to the page
+          subImages.appendChild(newSubImg);
+        });
+
+        // remove the colours we applied on the last click
+        theSubhead.classList.remove(appliedClass);
+        theHeading.classList.remove(appliedClass);
+
+        // change the text using the values of the properties in the object
+        theSubhead.firstChild.nodeValue = objectIndex.headline;
+        theSeasonText.firstChild.nodeValue = objectIndex.text;
+
+        // add the color that corresponds to the season we clicked on
+        theSubhead.classList.add(this.id);
+        theHeading.classList.add(this.id);
+
+        appliedClass = this.id;
       }
 
       theImages.forEach(function(image, index){
