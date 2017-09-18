c1da0786c519400f20df4d6e367dd329d221585c
Pozeg <s_pozeg@tss-hr420-dm32.local>
2017-09-18 16:27:36 -0400

    new script js folder

--- script/main.js
+++ script/main.js
@@ -1,1 +1,18 @@
+(function() {
+  var theImages = document.querySelectorAll('.image-holder'),
+      theHeading = document.querySelector('.heading'),
+      theSubHead = document.querySelector('.main-copy h2'),
+      theSeasonText = document.querySelector('.main-copy p'),
+      appliedClass;
 
+      // i want to change all the content on the page
+      function changeElements() {
+        debugger; // this is a special term that stops code execution
+      }
+
+      theImages.forEach(function(image, index){
+        //add an event handler to each image
+        image.addEventListener('click', changeElements, false);
+      });
+
+})();
